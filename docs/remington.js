9deadc959579c604ac6e16ef918677d21e660ac9
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2016-11-02 11:03:04 -0600

    Automated build commit

--- docs/remington.js
+++ docs/remington.js
@@ -45,20 +45,23 @@ var Remington =
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var Remington = module.exports = __webpack_require__(1);
+	'use strict';
 
+	var Remington = module.exports = __webpack_require__(1);
 
 /***/ },
 /* 1 */
 /***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
 
 	/**
 	 * The Remington module
 	 * @module remington
 	 */
 
-	const keycodes = __webpack_require__(2);
-	const constants = __webpack_require__(3);
+	var keycodes = __webpack_require__(2);
+	var constants = __webpack_require__(3);
 
 	/**
 	 * The Remington constructor
@@ -66,8 +69,8 @@ var Remington =
 	 * @param {Element} element - The DOM element to which to attach this Remington instance
 	 * @param {Function} [inputCallback] - A callback that fires whenever the Remington instance detects input. Takes a single parameter, the event that was fired
 	 */
-	const Remington = function(element, inputCallback) {
-	    const self = this;
+	var Remington = function Remington(element, inputCallback) {
+	    var self = this;
 
 	    /**
 	     * The buffer of the Remington instance
@@ -95,17 +98,17 @@ var Remington =
 	     * The current index into the buffer
 	     * @private
 	     */
-	    let currentIndex = 0;
+	    var currentIndex = 0;
 
 	    /**
 	     * Calculate the current index into the buffer based on cursor location
 	     * @private
 	     */
-	    const calculateCurrentIndex = function() {
-	        let index = 0;
-	        let newlinesVisited = 0;
+	    var calculateCurrentIndex = function calculateCurrentIndex() {
+	        var index = 0;
+	        var newlinesVisited = 0;
 	        // first, find the beginning of the current row
-	        while(newlinesVisited < self.cursor.row) {
+	        while (newlinesVisited < self.cursor.row) {
 	            index = self.buffer.text.indexOf(constants.NEWLINE, index);
 	            newlinesVisited++;
 	        }
@@ -122,28 +125,36 @@ var Remington =
 	     * @params {...String} [items] - Characters or strings to add to the string
 	     * @returns {String} The modified string
 	     */
-	    const spliceString = function(start, deleteCount, ...items) {
+	    var spliceString = function spliceString(start, deleteCount) {
+	        for (var _len = arguments.length, items = Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
+	            items[_key - 2] = arguments[_key];
+	        }
+
 	        if (typeof deleteCount === "string") {
 	            items.splice(0, 0, deleteCount);
 	            deleteCount = this.length - start;
 	        }
-	        const string = this.slice(0, start) + items.join('') + this.slice(start + deleteCount, this.length);
+	        var string = this.slice(0, start) + items.join('') + this.slice(start + deleteCount, this.length);
 	        return string;
-	    }
+	    };
 
 	    // Add splice as a non-enumerable read-only property of String.prototype
-	    Object.defineProperty(String.prototype, 'splice', {value: spliceString});
+	    Object.defineProperty(String.prototype, 'splice', { value: spliceString });
 
 	    /**
 	     * The keypress event listener handles inserting regular characters
 	     */
-	    element.addEventListener('keypress', function(event) {
-	        const character = String.fromCharCode(event.charCode);
-	        self.buffer.text = self.buffer.text.splice(currentIndex, 0, character);
-	        if (!rows[self.cursor.row]) {
-	            rows[self.cursor.row] = "";
+	    element.addEventListener('keypress', function (event) {
+	        // catch any keypress events that should be handled as non-character keys
+	        if (Object.values(keycodes).indexOf(event.keyCode) >= 0) {
+	            return;
 	        }
-	        rows[self.cursor.row] = rows[self.cursor.row].splice(self.cursor.col, 0, character);
+	        var character = String.fromCharCode(event.charCode);
+	        self.buffer.text = self.buffer.text.splice(currentIndex, 0, character);
+	        if (!self.buffer.rows[self.cursor.row]) {
+	            self.buffer.rows[self.cursor.row] = "";
+	        }
+	        self.buffer.rows[self.cursor.row] = self.buffer.rows[self.cursor.row].splice(self.cursor.col, 0, character);
 	        currentIndex++;
 	        self.cursor.col++;
 	        if (inputCallback && typeof inputCallback === 'function') {
@@ -154,58 +165,65 @@ var Remington =
 	    /**
 	     * The keydown event listener handles non-character keys such as ENTER
 	     */
-	    element.addEventListener('keydown', function(event) {
-	        switch(event.keyCode) {
-	            case keycodes.ENTER: {
-	                self.buffer.text = self.buffer.text.splice(currentIndex, 0, constants.NEWLINE);
-	                currentIndex++;
-	                if (!rows[self.cursor.row]) {
-	                    rows[self.cursor.row] = "";
+	    element.addEventListener('keydown', function (event) {
+	        var validKey = true;
+	        switch (event.keyCode) {
+	            case keycodes.ENTER:
+	                {
+	                    self.buffer.text = self.buffer.text.splice(currentIndex, 0, constants.NEWLINE);
+	                    currentIndex++;
+	                    if (!self.buffer.rows[self.cursor.row]) {
+	                        self.buffer.rows[self.cursor.row] = "";
+	                    }
+	                    var remainingRow = self.buffer.rows[self.cursor.row].slice(currentIndex, self.buffer.rows[self.cursor.row].length);
+	                    self.buffer.rows[self.cursor.row] = self.buffer.rows[self.cursor.row].splice(currentIndex, constants.NEWLINE);
+	                    self.buffer.rows.splice(self.cursor.row + 1, 0, remainingRow);
+	                    self.cursor.row++;
+	                    self.cursor.col = 0;
+	                    break;
 	                }
-	                const remainingRow = rows[self.cursor.row].slice(currentIndex, rows[self.cursor.row].length);
-	                rows[self.cursor.row] = rows[self.cursor.row].splice(currentIndex, constants.NEWLINE);
-	                rows.splice(self.cursor.row, 0, remainingRow);
-	                self.cursor.row++;
-	                self.cursor.col = 0;
-	                break;
-	            }
-	            case keycodes.BACKSPACE: {
-	                // BACKSPACE deletes the character before the currentIndex, unless currentIndex is 0
-	                if (currentIndex > 0) {
-	                    self.buffer = self.buffer.splice(currentIndex - 1, 1);
-	                    currentIndex--;
+	            case keycodes.BACKSPACE:
+	                {
+	                    // BACKSPACE deletes the character before the currentIndex, unless currentIndex is 0
+	                    if (currentIndex > 0) {
+	                        self.buffer.text = self.buffer.text.splice(currentIndex - 1, 1);
+	                        currentIndex--;
+	                    }
+	                    // if the cursor is at the beginning of a row, go the end of the previous row
+	                    if (self.cursor.col === 0 && self.cursor.row > 0) {
+	                        // concatenate the current row with the previous one
+	                        // and delete the newline at the end of the previous one
+	                        self.buffer.rows[self.cursor.row - 1] = self.buffer.rows[self.cursor.row - 1].splice(self.buffer.rows[self.cursor.row - 1].length - 1, 1, self.buffer.rows[self.cursor.row]);
+	                        self.buffer.rows.splice(self.cursor.row, 1);
+	                        self.cursor.row--;
+	                        self.cursor.col = self.buffer.rows[self.cursor.row].length;
+	                    }
+	                    // otherwise move the cursor back 1 column, unless it is at (0,0)
+	                    else if (!(self.cursor.col === 0 && self.cursor.row == 0)) {
+	                            self.buffer.rows[self.cursor.row] = self.buffer.rows[self.cursor.row].splice(self.cursor.col - 1, 1);
+	                            self.cursor.col--;
+	                        }
+	                    break;
 	                }
-	                // if the cursor is at the beginning of a row, go the end of the previous row
-	                if (self.cursor.col === 0 && self.cursor.row > 0) {
-	                    // concatenate the current row with the previous one
-	                    // and delete the newline at the end of the previous one
-	                    rows[self.cursor.row] = rows[self.cursor.row].splice(rows[self.cursor.row - 1].length - 1, 1, rows[self.cursor.row]);
-	                    self.cursor.row--;
-	                    self.cursor.col = rows[self.cursor.row].length - 1;
+	            default:
+	                {
+	                    validKey = false;
+	                    break;
 	                }
-	                // otherwise move the cursor back 1 column, unless it is at (0,0)
-	                else if (!(self.cursor.col === 0 && self.cursor.row == 0)) {
-	                    rows[self.cursor.row] = rows[self.cursor.row].splice(self.cursor.col - 1, 1);
-	                    self.cursor.col--;
-	                }
-	                break;
-	            }
-	            default: {
-	                break;
-	            }
 	        }
-	        if (inputCallback && typeof inputCallback === 'function') {
+	        if (validKey && inputCallback && typeof inputCallback === 'function') {
 	            inputCallback(event);
 	        }
 	    });
-	}
+	};
 
 	module.exports = Remington;
-
 
 /***/ },
 /* 2 */
 /***/ function(module, exports) {
+
+	"use strict";
 
 	/**
 	 * Keycode constants
@@ -217,17 +235,17 @@ var Remington =
 	    "TAB": 9,
 	    "CTRL": 17,
 	    "ALT": 18
-	}
-
+	};
 
 /***/ },
 /* 3 */
 /***/ function(module, exports) {
 
+	"use strict";
+
 	module.exports = {
 	    "NEWLINE": '\n'
-	}
-
+	};
 
 /***/ }
 /******/ ]);
